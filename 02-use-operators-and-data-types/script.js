3bf9c975ae7ee9ec0bec201205405fdc5a4b74ee
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-15 17:22:31 +0100

    Task2:operators use and data-types

---
+++ 02-use-operators-and-data-types/script.js
@@ -0,0 +1,43 @@
+//use-operators-and-data-types//
+
+//PART 1//
+console.log("V" + "a" + "n" + "e" + "s" + "s" + "a");
+
+//PART 2//
+// Error! Make it work
+//console.log("Hallo";//
+
+console.log("Hallo");
+
+// Error! Make it work
+//console.log("That doesn"t work");//
+
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+//console.log(50 + 2 > 52);//
+
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it.
+//console.log("333" == 333);//
+
+console.log(!"333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+//console.log(600 + 510 / 2);//
+
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+//console.log("2" + 2);//
+
+console.log(2 + 2);
+
+//PART 3//
+
+//Create a console.log that returns true when both division results are equal to 5//
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 && 50 / 10 === 5);
