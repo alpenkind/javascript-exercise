e7920209763a3568406cd45e30226a7668ac8a4a
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-02-18 22:25:13 +0100

    Task5:if-and-switch-statements

---
+++ 05-if-and-switch-statements/script.js
@@ -0,0 +1,69 @@
+//Task1
+
+const size = 25;
+let result;
+
+if (size > 10) {
+  result = "Greater than 10";
+}
+if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+//Task 2
+// TODO: Implement the oddEven function
+
+function oddEven(number) {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+//Task 3
+
+function oldYoung(number) {
+  if (number < 0) {
+    return "invalid";
+  }
+  if (number < 16) {
+    return "children";
+  }
+  if (number < 50) {
+    return "young person";
+  } else {
+    return "elder person";
+  }
+}
+
+// TODO: Implement the oldYoung function
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
